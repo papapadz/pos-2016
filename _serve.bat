64105c5ce844ba0f6f9c7734a59bebc9910f879a
Padz <benpadz08@gmail.com>
2022-08-30 22:44:48 +0800

    bug fixes

--- _serve.bat
+++ _serve.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome --kiosk http://127.0.0.1:8000 --disable-pinch --overscroll-history-navigation=0
+"C:\Users\Acer\AppData\Local\Programs\Opera\launcher.exe" --chrome --kiosk --args --incognito http://127.0.0.1:8000 --disable-pinch --overscroll-history-navigation=0
 cd C:\xampp\htdocs\pos-2016
 php artisan serve --host=127.0.0.1 --port=8000
 EXIT
